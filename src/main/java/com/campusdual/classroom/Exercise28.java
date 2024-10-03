f9e9f0fe24dc31918b70967fed24aa51c9ebe827
Chus <mjesus.mh.12@gmail.com>
2024-10-03 11:27:48 +0200

    Ejr.: 28 completado

--- src/main/java/com/campusdual/classroom/Exercise28.java
+++ src/main/java/com/campusdual/classroom/Exercise28.java
@@ -3,5 +3,7 @@ package com.campusdual.classroom;
 public class Exercise28 {
     public static void main(String[] args) {
 
+        throw new RuntimeException("This is a test exception");
+
     }
 }
